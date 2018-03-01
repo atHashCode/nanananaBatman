d0b7f2f02f632bace6eb58ed1550257572a39cfa
José Antonio Navarro Fuentes <janavarro.fuentes@atsistemas.com>
2018-03-01 19:13:31 +0100

    Carrera

---
+++ java/com/atsistemas/hashcode/batmaaaaan/Ride.java
@@ -0,0 +1,20 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Carrera {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Carrera.class);
+	Integer coordInicio[] = new Integer[2];
+	Integer coordFin[] = new Integer[2];
+	Integer inicio;
+	Integer fin;
+
+	public Carrera(Integer coordInicioRow, Integer coordInicioCol,Integer coordFinRow,Integer coordFinCol,Integer tiempoInicio,Integer Fin) {
+		coordInicio[0] = coordInicioRow;
+		coordInicio[1] = coordInicioCol;
+		coordFin[0] = coordFinRow;
+		coordFin[1] = coordFinCol;
+		inicio = tiempoInicio;
+		fin = tiempoFin;
+	}
+
+}
