30148c6c2aed0476545da0a90587729b263b4321
Ruben Gil <rgil@atsistemas.com>
2018-03-01 20:40:16 +0100

    FIX setter

--- src/main/java/com/atsistemas/hashcode/batmaaaaan/Posicion.java
+++ src/main/java/com/atsistemas/hashcode/batmaaaaan/Posicion.java
@@ -11,17 +11,13 @@ public class Posicion {
 	}
 
 	
-		public int getX() {
-			return x;
-		}
-		public void setX(int x) {
-			this.x = x;
-		}
-
+	public int getX() {
+		return x;
+	}
+	
 	public void setX(int x) {
 		this.x = x;
 	}
-
 
 	public int getY() {
 		return y;
